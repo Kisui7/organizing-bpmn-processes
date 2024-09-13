739ccfd4e5d999374c3af1b47daf7055111a5a9d
Adonay <adonay.rssantos@gmail.com>
2024-09-13 18:26:12 -0300

    Criação base do Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,13 @@ export default function Header() {
   return (
     <>
       <header className="header">
-        <div className="logo">
-          <h1>SouthStar</h1>
-        </div>
-        <div className="links-nav">
+      <div className="links-nav">
           <Link to="/">Home</Link>
           <Link to="/login">Login</Link>
           <Link to="/cadastro">Cadastro</Link>
+        </div>
+        <div className="logo">
+          <h1>SouthStar</h1>
         </div>
       </header>
     </>
